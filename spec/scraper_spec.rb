ba4254dd0b42973536010108a762035d8f583181
NathanNT <55759030+NathanNT@users.noreply.github.com>
2019-10-11 02:39:34 +0200

    Add files via upload

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -4,15 +4,8 @@ require_relative '../lib/scraper'
 
   describe "#testing" do
 
-    it "Give me the key thank to the value" do
-      expect(testing(0)).to eq("Bitcoin"=>"$8583.13"}')
-    end
-
-    it "Give me the key thank to the value" do
-      expect(testing(1)).to eq("Ethereum"=>"$191.71")
-    end
+  
     it "should displays more than 2000 hashes" do
     	expect(scraper.length > 200).to eq(true)
     end
-
-    end
+end
